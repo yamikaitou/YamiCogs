c6a884838372c19e8aa71c0ecc37ea34076c59bf
YamiKaitou <yamikaitou@gmail.com>
2020-06-06 20:50:25 -0400

    obtain lang file

--- backend-scripts/mccasino/parse.py
+++ backend-scripts/mccasino/parse.py
@@ -1,10 +1,14 @@
 import requests
 import zipfile
 import os
+import shutil
+import json
 
 os.chdir(os.path.dirname(__file__))
 if os.path.exists("server.jar"):
     os.remove("server.jar")
+if os.path.exists("assets"):
+    shutil.rmtree('assets')
 
 r = requests.get("https://launchermeta.mojang.com/mc/game/version_manifest.json")
 versions = r.json()
@@ -22,4 +26,11 @@ r = requests.get(jar, allow_redirects=True)
 open('server.jar', 'wb').write(r.content)
 
 with zipfile.ZipFile("server.jar") as z:
-    z.extract("assets\minecraft\lang\en_us.json")
+    z.extract("assets/minecraft/lang/en_us.json")
+
+with open("assets/minecraft/lang/en_us.json") as f:
+    data = json.load(f)
+
+for key,value in data.items():
+    if key[:16] == "block.minecraft." and key[:23] != "block.minecraft.banner.":
+        print(f"{key[16:]} - {value}")
