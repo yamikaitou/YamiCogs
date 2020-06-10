d018c8d12b761fbf01afc7d154a5d54689d766aa
YamiKaitou <yamikaitou@gmail.com>
2020-06-09 20:51:02 -0400

    changing computers can be a pain sometimes

--- backend-scripts/pogo/parse.py
+++ backend-scripts/pogo/parse.py
@@ -1,24 +1,26 @@
 import requests
-import apsw
+import json
 import re
+import os
 
 
-sql = apsw.Connection("pogo/data/pkmn.db")
+os.chdir(os.path.dirname(__file__))
+if os.path.exists("../../pogo/data/pkmn.json"):
+    os.remove("../../pogo/data/pkmn.json")
+    with open("../../pogo/data/pkmn.json", "w") as f:
+        json.dump({"version":"0"}, f)
+with open("../../pogo/data/pkmn.json") as f:
+    data = json.load(f)
 
-cursor = sql.cursor()
-for version, in cursor.execute("SELECT value FROM settings WHERE key = 'game_master'"):
-    r = requests.get("https://raw.githubusercontent.com/pokemongo-dev-contrib/pokemongo-game-master/master/versions/latest-version.txt")
-    if version >= r.text:
-        sql.close()
-        exit(0)
+r = requests.get("https://raw.githubusercontent.com/pokemongo-dev-contrib/pokemongo-game-master/master/versions/latest-version.txt")
+if data['version'] >= r.text:
+    exit(0)
 
 r = requests.get("https://raw.githubusercontent.com/pokemongo-dev-contrib/pokemongo-game-master/master/versions/latest/V2_GAME_MASTER.json")
 master = r.json()
 
-repkmn = re.compile('^[V][0-9]\S+(_POKEMON_)')
+repkmn = re.compile('^(FORMS_)[V][0-9]\S+(_POKEMON_)')
 
 for entry in master['template']:
     if repkmn.match(entry['templateId']):
-        print(entry['templateId'])
-
-sql.close()
+        print(entry['data']['formSettings']['pokemon'])
