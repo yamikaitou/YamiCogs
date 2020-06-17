f87fe7f3bebe28d6f99e91c9ea880a90cf5679d8
YamiKaitou <yamikaitou@gmail.com>
2020-06-17 17:08:03 -0400

    I do nothing still

--- mccasino/core.py
+++ mccasino/core.py
@@ -11,3 +11,23 @@ class MinecraftCasino(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=582650109, force_registration=True)
+    
+    @commands.group()
+    @checks.guildowner_or_permissions(manage_guild=True)
+    async def mccasino(self, ctx):
+        """Configure the Casino"""
+        pass
+
+    @mccasino.command(name="info")
+    async def mcc_info(self, ctx):
+        """View configured settings"""
+        pass
+    
+    @commands.group()
+    async def mcpoker(self, ctx):
+        """Participate in a betting game using randomly generated Minecraft Statistics"""
+        pass
+    
+    @mcpoker.command(name="start")
+    async def mcp_start(self, ctx):
+        pass
