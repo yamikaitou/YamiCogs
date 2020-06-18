83f9b27c638f847f5afcd791c018c57149b0acdb
Ryan <yamikaitou@gmail.com>
2020-06-17 22:29:33 -0400

    a start to a default config

--- mccasino/core.py
+++ mccasino/core.py
@@ -11,6 +11,13 @@ class MinecraftCasino(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=582650109, force_registration=True)
+
+        default_guild = {
+            "conversion": 100,
+            "poker_cost": 45
+        }
+
+        self.config.register_guild(**default_guild)
     
     @commands.group()
     @checks.guildowner_or_permissions(manage_guild=True)
@@ -30,4 +37,10 @@ class MinecraftCasino(commands.Cog):
     
     @mcpoker.command(name="start")
     async def mcp_start(self, ctx):
+        """Start a game"""
+        pass
+
+    @mcpoker.command(name="join")
+    async def mcp_join(self, ctx):
+        """Join a game"""
         pass
