0cde379d1ee852c24784cd7ccc3f5b67b669b49d
Ryan <git@yamikaitou.dev>
2022-12-06 03:28:18 +0000

    remove abandoned projects

--- mccasino/__init__.py
+++
@@ -1,5 +0,0 @@
-from .mccasino import MinecraftCasino
-
-
-def setup(bot):
-    bot.add_cog(MinecraftCasino(bot))
--- pogo/__init__.py
+++
@@ -1,5 +0,0 @@
-from .pogo import PokemonGo
-
-
-def setup(bot):
-    bot.add_cog(PokemonGo(bot))
