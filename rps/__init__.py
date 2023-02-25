dc17c3029d9e61c976715eec9bccd210460d4b58
Ryan <git@yamikaitou.dev>
2023-02-25 12:26:32 -0500

    rps update

--- rps/__init__.py
+++ rps/__init__.py
@@ -2,8 +2,6 @@ import json
 from asyncio import create_task
 from pathlib import Path
 
-from dislash import InteractionClient
-
 from .rps import RPS
 
 with open(Path(__file__).parent / "info.json") as fp:
@@ -14,9 +12,9 @@ old_rps = None
 # Thanks flare
 
 
-def setup(bot):
-    create_task(setup_after_ready(bot))
 
+async def setup(bot):
+    create_task(setup_after_ready(bot))
 
 async def setup_after_ready(bot):
     global old_rps
@@ -27,10 +25,8 @@ async def setup_after_ready(bot):
     if old_rps:
         bot.remove_command(old_rps.name)
 
-    bot.add_cog(cog)
-    if not hasattr(bot, "slash"):
-        bot.slash = InteractionClient(bot, sync_commands=False)
+    await bot.add_cog(cog)
 
 
-def teardown(bot):
+async def teardown(bot):
     bot.add_command(old_rps)
