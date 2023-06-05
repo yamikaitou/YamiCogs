f000e526ca38724b552668ce667ee91d4a1f5984
Ryan <git@yamikaitou.dev>
2023-06-05 03:04:19 +0000

    Red 3.5 Changes

--- Tube/__init__.py
+++ Tube/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from .tube import Tube
 
+
 async def setup(bot):
-    bot.add_cog(Tube(bot))
+    await bot.add_cog(Tube(bot))
--- Tube/tube.py
+++ Tube/tube.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
-import asyncio
 import datetime
-import re
-import time
 import hashlib
 import logging
+import re
+import time
+from typing import Optional
 
 import aiohttp
 import discord
 import feedparser
-
-from typing import Optional
-
 from discord.ext import tasks
 from redbot.core import Config, bot, checks, commands
 from redbot.core.utils.chat_formatting import pagify
@@ -20,20 +17,23 @@ log = logging.getLogger("red.cbd-cogs.tube")
 
 __all__ = ["UNIQUE_ID", "Tube"]
 
-UNIQUE_ID = 0x547562756c6172
+UNIQUE_ID = 0x547562756C6172
 
 TIME_DEFAULT = "1970-01-01T00:00:00+00:00"
 # Time tuple for use with time.mktime()
 TIME_TUPLE = (*(int(x) for x in re.split(r"-|T|:|\+", TIME_DEFAULT)), 0)
 
 # Word tokenizer
-TOKENIZER = re.compile(r'([^\s]+)')
+TOKENIZER = re.compile(r"([^\s]+)")
+
 
 class Tube(commands.Cog):
     """A YouTube subscription cog
 
     Thanks to mikeshardmind(Sinbad) for the RSS cog as reference"""
+
     has_warned_about_invalid_channels = False
+
     def __init__(self, bot: bot.Red):
         self.bot = bot
         self.conf = Config.get_conf(self, identifier=UNIQUE_ID, force_registration=True)
@@ -44,12 +44,17 @@ class Tube(commands.Cog):
     @commands.group()
     async def tube(self, ctx: commands.Context):
         """Post when new videos are added to a YouTube channel"""
-        pass
 
     @checks.admin_or_permissions(manage_guild=True)
     @commands.guild_only()
     @tube.command()
-    async def subscribe(self, ctx: commands.Context, channelYouTube, channelDiscord: Optional[discord.TextChannel] = None, publish: Optional[bool] = False):
+    async def subscribe(
+        self,
+        ctx: commands.Context,
+        channelYouTube,
+        channelDiscord: Optional[discord.TextChannel] = None,
+        publish: Optional[bool] = False,
+    ):
         """Subscribe a Discord channel to a YouTube channel
 
         If no discord channel is specified, the current channel will be subscribed
@@ -67,16 +72,17 @@ class Tube(commands.Cog):
         if not channelDiscord:
             channelDiscord = ctx.channel
         subs = await self.conf.guild(ctx.guild).subscriptions()
-        newSub = {'id': channelYouTube,
-                  'channel': {'name': channelDiscord.name,
-                              'id': channelDiscord.id},
-                  'publish': publish}
-        newSub['uid'] = self.sub_uid(newSub)
+        newSub = {
+            "id": channelYouTube,
+            "channel": {"name": channelDiscord.name, "id": channelDiscord.id},
+            "publish": publish,
+        }
+        newSub["uid"] = self.sub_uid(newSub)
         for sub in subs:
-            if sub['uid'] == newSub['uid']:
+            if sub["uid"] == newSub["uid"]:
                 await ctx.send("This subscription already exists!")
                 return
-        feed = feedparser.parse(await self.get_feed(newSub['id']))
+        feed = feedparser.parse(await self.get_feed(newSub["id"]))
         last_video = {}
         for entry in feed["entries"]:
             if not last_video or entry["published_parsed"] > last_video["published_parsed"]:
@@ -95,18 +101,23 @@ class Tube(commands.Cog):
     @checks.admin_or_permissions(manage_guild=True)
     @commands.guild_only()
     @tube.command()
-    async def unsubscribe(self, ctx: commands.Context, channelYouTube, channelDiscord: Optional[discord.TextChannel] = None):
+    async def unsubscribe(
+        self,
+        ctx: commands.Context,
+        channelYouTube,
+        channelDiscord: Optional[discord.TextChannel] = None,
+    ):
         """Unsubscribe a Discord channel from a YouTube channel
 
-        If no Discord channel is specified and the asAnnouncement flag not set to True, the subscription will be removed from all channels"""
+        If no Discord channel is specified and the asAnnouncement flag not set to True, the subscription will be removed from all channels
+        """
         subs = await self.conf.guild(ctx.guild).subscriptions()
         unsubbed = []
         if channelDiscord:
-            newSub = {'id': channelYouTube,
-                      'channel': {'id': channelDiscord.id}}
-            unsubTarget, unsubType = self.sub_uid(newSub), 'uid'
+            newSub = {"id": channelYouTube, "channel": {"id": channelDiscord.id}}
+            unsubTarget, unsubType = self.sub_uid(newSub), "uid"
         else:
-            unsubTarget, unsubType = channelYouTube, 'id'
+            unsubTarget, unsubType = channelYouTube, "id"
         for i, sub in enumerate(subs):
             if sub[unsubType] == unsubTarget:
                 unsubbed.append(subs.pop(i))
@@ -120,7 +131,7 @@ class Tube(commands.Cog):
     @commands.guild_only()
     @tube.command()
     async def customize(self, ctx: commands.Context, channelYouTube, customMessage: str = False):
-        """ Add a custom message to videos from a YouTube channel
+        """Add a custom message to videos from a YouTube channel
 
         You can use any keys available in the RSS object in your custom message
         by surrounding the key in perecent signs, e.g.:
@@ -131,9 +142,9 @@ class Tube(commands.Cog):
         subs = await self.conf.guild(ctx.guild).subscriptions()
         found = False
         for i, sub in enumerate(subs):
-            if sub['id'] == channelYouTube:
+            if sub["id"] == channelYouTube:
                 found = True
-                subs[i]['custom'] = customMessage
+                subs[i]["custom"] = customMessage
         if not found:
             await ctx.send("Subscription not found")
             return
@@ -143,14 +154,16 @@ class Tube(commands.Cog):
     @checks.admin_or_permissions(manage_guild=True)
     @commands.guild_only()
     @tube.command()
-    async def rolemention(self, ctx: commands.Context, channelYouTube, rolemention: Optional[discord.Role]):
-        """ Adds a role mention in front of the message """
+    async def rolemention(
+        self, ctx: commands.Context, channelYouTube, rolemention: Optional[discord.Role]
+    ):
+        """Adds a role mention in front of the message"""
         subs = await self.conf.guild(ctx.guild).subscriptions()
         found = False
         for i, sub in enumerate(subs):
-            if sub['id'] == channelYouTube:
+            if sub["id"] == channelYouTube:
                 found = True
-                subs[i]['mention'] = rolemention.id
+                subs[i]["mention"] = rolemention.id
         if not found:
             await ctx.send("Subscription not found")
             return
@@ -171,12 +184,12 @@ class Tube(commands.Cog):
         subs_by_channel = {}
         for sub in subs:
             # Channel entry must be max 124 chars: 103 + 2 + 18 + 1
-            channel = f'{sub["channel"]["name"][:103]} ({sub["channel"]["id"]})' # Max 124 chars
+            channel = f'{sub["channel"]["name"][:103]} ({sub["channel"]["id"]})'  # Max 124 chars
             subs_by_channel[channel] = [
                 # Sub entry must be max 100 chars: 45 + 2 + 24 + 4 + 25 = 100
                 f"{sub.get('name', sub['id'][:45])} ({sub['id']}) - {sub.get('previous', 'Never')}",
                 # Preserve previous entries
-                *subs_by_channel.get(channel, [])
+                *subs_by_channel.get(channel, []),
             ]
         if ctx.channel.permissions_for(guild.me).embed_links:
             for channel, sub_ids in subs_by_channel.items():
@@ -192,7 +205,7 @@ class Tube(commands.Cog):
                         page += 1
                     embed.title = title
                     await ctx.send(embed=embed)
-                    del(sub_ids[0:9])
+                    del sub_ids[0:9]
         else:
             subs_string = ""
             for channel, sub_ids in subs_by_channel.items():
@@ -248,7 +261,13 @@ class Tube(commands.Cog):
                 continue
             fetched.update(update)
 
-    async def _get_new_videos(self, guild: discord.Guild, cache: dict = {}, ctx: commands.Context = None, demo: bool = False):
+    async def _get_new_videos(
+        self,
+        guild: discord.Guild,
+        cache: dict = {},
+        ctx: commands.Context = None,
+        demo: bool = False,
+    ):
         try:
             subs = await self.conf.guild(guild).subscriptions()
             history = await self.conf.guild(guild).cache()
@@ -276,18 +295,20 @@ class Tube(commands.Cog):
             last_video_time = datetime.datetime.fromtimestamp(
                 time.mktime(
                     time.strptime(
-                        sub.get("previous", "1970-01-01T00:00:00+00:00"),
-                        "%Y-%m-%dT%H:%M:%S%z"
+                        sub.get("previous", "1970-01-01T00:00:00+00:00"), "%Y-%m-%dT%H:%M:%S%z"
                     )
                 )
             )
             for entry in cache[sub["id"]]["entries"][::-1]:
-                published = datetime.datetime.fromtimestamp(time.mktime(entry.get("published_parsed", TIME_TUPLE)))
+                published = datetime.datetime.fromtimestamp(
+                    time.mktime(entry.get("published_parsed", TIME_TUPLE))
+                )
                 if not sub.get("name"):
                     altered = True
                     sub["name"] = entry["author"]
-                if ((published > last_video_time and not entry["yt_videoid"] in history)
-                    or (demo and published > last_video_time - datetime.timedelta(seconds=1))):
+                if (published > last_video_time and not entry["yt_videoid"] in history) or (
+                    demo and published > last_video_time - datetime.timedelta(seconds=1)
+                ):
                     altered = True
                     subs[i]["previous"] = entry["published"]
                     new_history.append(entry["yt_videoid"])
@@ -304,8 +325,10 @@ class Tube(commands.Cog):
                             # Let the embed provide necessary info
                             description = entry["link"]
                         else:
-                            description = (f"New video from *{entry['author'][:500]}*:"
-                                           f"\n**{entry['title'][:500]}**\n{entry['link']}")
+                            description = (
+                                f"New video from *{entry['author'][:500]}*:"
+                                f"\n**{entry['title'][:500]}**\n{entry['link']}"
+                            )
 
                     mention_id = sub.get("mention", False)
                     if mention_id:
@@ -362,12 +385,11 @@ class Tube(commands.Cog):
         """Fetch data from a feed"""
         async with aiohttp.ClientSession() as session:
             res = await self.fetch(
-                session,
-                f"https://www.youtube.com/feeds/videos.xml?channel_id={channel}"
+                session, f"https://www.youtube.com/feeds/videos.xml?channel_id={channel}"
             )
         return res
 
-    def cog_unload(self):
+    async def cog_unload(self):
         self.background_get_new_videos.cancel()
 
     @tasks.loop(seconds=1)
