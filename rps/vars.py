0b09b0eddf742ffc0b6e05b5a3c048e929e28951
Ryan <git@yamikaitou.dev>
2024-04-12 03:05:58 +0000

    fix RPS enum for 3.11

--- rps/vars.py
+++ rps/vars.py
@@ -5,12 +5,24 @@ class Result(str, Enum):
     LOSE = "Look at that, I win!"
     WIN = "Congrats, you win!"
     TIE = "Well, we must be mind-readers!"
+    
+    def __str__(self) -> str:
+        return self.value
+
+    def __int__(self) -> int:
+        return self.value
 
 
 class RPSChoice(Enum):
     ROCK = 1
     PAPER = 2
     SCISSORS = 3
+    
+    def __str__(self) -> str:
+        return self.value
+
+    def __int__(self) -> int:
+        return self.value
 
 
 class RPSLSChoice(Enum):
@@ -19,12 +31,24 @@ class RPSLSChoice(Enum):
     SCISSORS = 3
     LIZARD = 4
     SPOCK = 5
+    
+    def __str__(self) -> str:
+        return self.value
+
+    def __int__(self) -> int:
+        return self.value
 
 
 class RPSIcon(str, Enum):
     ROCK = "\U0001faa8"
     PAPER = ("\N{NEWSPAPER}",)
     SCISSORS = "\N{BLACK SCISSORS}\N{VARIATION SELECTOR-16}"
+    
+    def __str__(self) -> str:
+        return self.value
+
+    def __int__(self) -> int:
+        return self.value
 
 
 class RPSLSIcon(str, Enum):
@@ -33,3 +57,9 @@ class RPSLSIcon(str, Enum):
     SCISSORS = "\N{BLACK SCISSORS}\N{VARIATION SELECTOR-16}"
     LIZARD = "\N{LIZARD}"
     SPOCK = "\N{RAISED HAND WITH PART BETWEEN MIDDLE AND RING FINGERS}"
+    
+    def __str__(self) -> str:
+        return self.value
+
+    def __int__(self) -> int:
+        return self.value
