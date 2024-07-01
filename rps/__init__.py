daa6cf26e74a226c5f7d20434ca42991b5d2630b
Ryan <git@yamikaitou.dev>
2024-07-01 17:19:03 +0000

    changed how the command is removed/restored

--- rps/__init__.py
+++ rps/__init__.py
@@ -9,24 +9,17 @@ with open(Path(__file__).parent / "info.json") as fp:
 
 old_rps = None
 
-# Thanks flare
-
-
 async def setup(bot):
-    create_task(setup_after_ready(bot))
-
-
-async def setup_after_ready(bot):
     global old_rps
-    await bot.wait_until_red_ready()
-    cog = RPS(bot)
-
     old_rps = bot.get_command("rps")
     if old_rps:
         bot.remove_command(old_rps.name)
-
+    
+    cog = RPS(bot)
     await bot.add_cog(cog)
 
-
 async def teardown(bot):
-    bot.add_command(old_rps)
+    global old_rps
+    if old_rps:
+        bot.remove_command("rps")
+        bot.add_command(old_rps)
